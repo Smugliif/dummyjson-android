d7d4b0138b8959e72dcd014ac2682fb6c308853c
Smugliif <valtteri.havula123@gmail.com>
2023-05-10 23:02:10 +0300

    feat: OkHttp fetch of users implemented

--- app/src/main/java/fi/tuni/dummyjsonusers/MainActivity.kt
+++ app/src/main/java/fi/tuni/dummyjsonusers/MainActivity.kt
@@ -1,9 +1,12 @@
 package fi.tuni.dummyjsonusers
 
+import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -11,6 +14,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import fi.tuni.dummyjsonusers.ui.theme.DummyJSONUsersTheme
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,15 +29,54 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     Greeting("Android")
+                    Button(onClick = { fetchUsers() }) {
+                        Text("Fetch Users")
+                    }
                 }
             }
         }
     }
 }
 
+data class User(
+    val id: Int,
+    val firstName: String,
+    val lastName: String
+)
+
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
+}
+
+fun fetchUsers() {
+    val url = "https://dummyjson.com/users"
+    val client = OkHttpClient()
+    val request = Request.Builder()
+        .url(url)
+        .build()
+    client.newCall(request).enqueue(object: Callback {
+        @Override
+        override fun onFailure(call: Call, e: IOException) {
+            Log.d("DEBUG","Error while fetching users")
+        }
+        @Override
+        override fun onResponse(call: Call, response: Response) {
+            if (!response.isSuccessful) {
+                Log.d("DEBUG","Something went wrong")
+                return
+            }
+            Log.d("DEBUG","Success")
+            val json = response.body?.string()
+            Log.d("DEBUG", json!!)
+            if (json != null) {
+                val jsonObject = JSONObject(json)
+                Log.d("DEBUG","json not null")
+            }
+        }
+    })
+    val users = mutableListOf<User>()
+
 }
 
 @Preview(showBackground = true)
